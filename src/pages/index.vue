625bb1cd71ddbd3b0ccb180d95ad3914273af3ae
orvice L <orvice@gmail.com>
2025-05-20 23:43:24 +0800

    Init

---
+++ src/pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-container class="fill-height d-flex flex-column justify-center align-center text-center">
+    <v-icon class="mb-6" color="primary" size="96">mdi-tools</v-icon>
+    <h1 class="text-h3 font-weight-bold mb-4">网站维护中</h1>
+    <p class="text-subtitle-1 mb-6">我们正在进行系统维护，稍后回来访问。感谢您的理解与支持！</p>
+    <v-divider class="my-4" style="width: 200px; margin: 0 auto;" />
+  </v-container>
+</template>
+
+<script setup>
+  //
+</script>
